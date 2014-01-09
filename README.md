99ff82fd1d0a44a907483f86f01174ac7f1227e8
Steve Perkins <stereosteve@gmail.com>
2014-01-09 13:31:32 -0500

    initial

---
+++ README.md
@@ -0,0 +1,35 @@
+# Jumbotron
+
+Fill your background with a video.
+
+A simple jQuery plugin to scale and center a video to fill the background of a webpage.
+
+
+
+## Usage
+
+1. Create a wrapper div with a video inside
+
+```html
+<div id="jumbo">
+  <video src="http://vjs.zencdn.net/v/oceans.mp4" autoplay loop muted></video>
+</div>
+```
+
+2. Call `jumbotron()` on the wrapper element
+
+```js
+// default
+$("#jumbo").jumbotron()
+
+// with options
+$("#jumbo").jumbotron({interval: 500})
+```
+
+
+
+## Notes
+
+The plugin will resize the wrapper to fill the screen and make the wrapper fixed position with a z-index of -100.  If your document is taller than the window, the video will stay fixed in the background as you scroll.
+
+The plugin also sets a default interval of 500ms to resize the video.  This handles both video loading and window resizing.  If the video takes a seconds to load, the interval will pick up the aspect ratio when the video loads and resize to fill screen.  Similarly when the user resizes the window the interval will resize the video to fill new window size.  You can configure this interval with the `interval` option.
---
+++ jumbotron.js
@@ -0,0 +1,52 @@
+$.fn.jumbotron = function(opts) {
+  opts = opts || {}
+  opts.interval = opts.interval || 500
+
+  var win = $(window)
+  var wrap = this
+  var vid = wrap.find('video')
+
+  if (vid.length == 0) {
+    throw new Error("You must call backgroundVideo on a wrapper div that contains a video element. No video element found.")
+  }
+
+  wrap.css({
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    'z-index': -100,
+    overflow: 'hidden',
+  })
+
+  function resize() {
+    var winAspect = win.width() / win.height()
+    var vidAspect = vid.width() / vid.height()
+    var scaleHeight = winAspect < vidAspect
+
+    if (scaleHeight) {
+      var newWidth = vidAspect * win.height()
+      var leftover = newWidth - win.width()
+      var margLeft = -1 * leftover / 2
+      vid.css({
+        width: newWidth,
+        height: win.height(),
+        'margin-top': '0',
+        'margin-left': margLeft,
+      })
+    } else {
+      var newHeight = vidAspect * win.width()
+      var leftover = newHeight - win.height()
+      var margTop = -1 * leftover / 2
+      vid.css({
+        width: win.width(),
+        height: newHeight,
+        'margin-top': margTop,
+        'margin-left': '0',
+      })
+    }
+  }
+
+  setInterval(resize, opts.interval);
+}
